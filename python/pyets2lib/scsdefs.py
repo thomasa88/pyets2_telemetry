9e8d1cda0426312b9303acb0bb2e9478e43f958a
Thomas Axelsson <thomasa88@gmail.com>
2019-07-22 18:40:23 +0200

    License information and copyright

--- python/pyets2lib/scsdefs.py
+++ python/pyets2lib/scsdefs.py
@@ -1,3 +1,22 @@
+#
+# Copyright 2019 Thomas Axelsson <thomasa88@gmail.com>
+#
+# This file is part of pyets2_telemetry.
+#
+# pyets2_telemetry is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyets2_telemetry is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyets2_telemetry.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 SCS_CHANNELS = []
 
 class ScsChannel(object):
